3e420a7e0536a36f50dd081b1ca8f77dc6c22798
Sazzad-Islam <66383609+Sazzad-Islam@users.noreply.github.com>
2020-07-26 15:07:50 +0600

    Replaced Jquery with Vanila Javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// var presentValue = document.getElementById('result').value
+
+function popUp(){
+    alert("We love Mehedy Sir")
+}
+
+
+function insertNumber(number){
+
+   document.getElementById('result').value = document.getElementById('result').value + number ;
+}
+
+function clearResult(){
+
+    document.getElementById('result').value = '' ;
+}
+
+function calculate(){
+   
+    document.getElementById('result').value = eval(document.getElementById('result').value) ;
+}
+
+
+function deleteNumber(){
+
+    var presentValue = document.getElementById('result').value ;
+    if(presentValue!=''){
+        document.getElementById('result').value = presentValue.slice(0,-1) ;
+    }
+}
+
+
+
